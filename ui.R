c766d139db74e2a462b69212ace19c29c106e54f
Francesco <francesco@Portatirancesco.station>
2020-04-22 12:06:49 +0200

    adding navbar

--- ui.R
+++ ui.R
@@ -1,13 +1,30 @@
 library(shinyWidgets)
+library(shinythemes)
+
+navbarPage(theme=shinytheme("sandstone"),
 
 
-fluidPage(
-  titlePanel("My Library"),
-fluidRow(
+
+
+
+
+#  setBackgroundColor(color = c("rgb(250,0,85)", "rgb(246,246,246)"),gradient = "linear",direction = "bottom"
+#    ),
+
+
+
+  title="Libreria di Francesco",
+  tabPanel("My Library",
+  mainPanel(
+    fluidRow(
       column(4,uiOutput("authors")),
       column(4,uiOutput("editors"))
+    ),
+    # Create a new row for the table.
+    DT::dataTableOutput("table")
+  )
 ),
-  # Create a new row for the table.
-  DT::dataTableOutput("table")
-#)
+  tabPanel("Statistics",
+  print("daje")
+)
 )
