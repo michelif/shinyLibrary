c7d7d59da254388bfa238f75bec7bd9833ac6772
Francesco <francesco@Portatirancesco.station>
2020-04-23 16:21:13 +0200

    improving page

--- server.R
+++ server.R
@@ -4,16 +4,15 @@ library(shiny)
 function(input, output) {
 
   mydata <- reactive({
-    data <- read.csv(file = './data/libreria.csv',stringsAsFactors=F)
     if (is.null(data)){
       return(NULL)
     }
-
+    data$year_read=gsub(",","",gsub(" ","",str_extract(data$Stato.di.lettura,"20.[0-9],")))
     return(data)
   })
 
-#output$data<-mydata()
-  # Filter data based on selections
+
+ # Filter data based on selections
 
 output$authors<-renderUI({
   data<-mydata()
@@ -30,10 +29,15 @@ output$editors<-renderUI({
 output$table <- DT::renderDataTable(DT::datatable({
   req(input$edit)
   data<-mydata()
-  if (input$edit!="All")data <- data[data$Editore == input$edit,]
-  if (input$aut!="All")data <- data[data$Autore == input$aut,]
-  data
+  dataRed <- data[,c(2,3,4,5,6,7,8)]
+  if (input$edit!="All")dataRed <- dataRed[dataRed$Editore == input$edit,]
+  if (input$aut!="All")dataRed <- dataRed[dataRed$Autore == input$aut,]
+  dataRed
 }))
 
+output$summary<-DT::renderDataTable(DT::datatable({
+  data<-mydata()
+  aggregate(Numero.di.pagine ~ year_read, data,FUN=sum)
+}))
 
 }
--- ui.R
+++ ui.R
@@ -1,30 +1,26 @@
 library(shinyWidgets)
 library(shinythemes)
-
-navbarPage(theme=shinytheme("sandstone"),
-
+library(stringr)
 
 
 
 
 
-#  setBackgroundColor(color = c("rgb(250,0,85)", "rgb(246,246,246)"),gradient = "linear",direction = "bottom"
-#    ),
-
-
-
-  title="Libreria di Francesco",
-  tabPanel("My Library",
-  mainPanel(
-    fluidRow(
-      column(4,uiOutput("authors")),
-      column(4,uiOutput("editors"))
-    ),
-    # Create a new row for the table.
-    DT::dataTableOutput("table")
+navbarPage("Libreria di Francesco",
+           tabPanel("My Library",
+                        fluidRow(
+                            column(4,uiOutput("authors")),
+                            column(4,uiOutput("editors"))
+                           ),
+                                        # Create a new row for the table.
+           DT::dataTableOutput("table",width = "150%")
+           ),
+           tabPanel("Statistics",
+                    DT::dataTableOutput("summary")
+                    ),
+                                        #                 setBackgroundColor(color = c("rgb(166, 182, 224)", "rgb(246,246,246)"),gradient = "linear",direction = "top")
+           setBackgroundColor(color = c("rgb(201, 212, 242)", "rgb(246,246,246)"),gradient = "radial",direction = "bottom")
   )
-),
-  tabPanel("Statistics",
-  print("daje")
-)
-)
+
+
+
